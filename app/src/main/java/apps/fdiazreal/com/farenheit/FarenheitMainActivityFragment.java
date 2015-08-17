478b0b2f7564e60bafc8d72d3bc6a1780f056ab5
Federico Diaz Real <federico.real@mercadolibre.com>
2015-08-16 21:19:11 -0300

    First commit

---
+++ app/src/main/java/apps/fdiazreal/com/farenheit/FarenheitMainActivityFragment.java
@@ -0,0 +1,23 @@
+package apps.fdiazreal.com.farenheit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FarenheitMainActivityFragment extends Fragment {
+
+    public FarenheitMainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_farenheit_main, container, false);
+    }
+}
