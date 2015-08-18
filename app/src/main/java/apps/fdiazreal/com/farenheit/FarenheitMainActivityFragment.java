998f7575de810f01b884901e1192caed42881115
Federico Diaz Real <federico.real@mercadolibre.com>
2015-08-18 01:58:03 -0300

    Adding a ListView to the fragment with mock data

--- app/src/main/java/apps/fdiazreal/com/farenheit/FarenheitMainActivityFragment.java
+++ app/src/main/java/apps/fdiazreal/com/farenheit/FarenheitMainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -18,6 +20,34 @@ public class FarenheitMainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_farenheit_main, container, false);
+
+        View fragmentView = inflater.inflate(R.layout.fragment_farenheit_main, container, false);
+
+        String[] forecasts = {
+            "Today - Sunny - 30/32",
+            "Tomorrow - Sunny - 30/32",
+            "20.08 - Sunny - 30/32",
+            "21.08 - Windy - 26/28",
+            "22.08 - Sunny - 28/30",
+            "23.08 - Storm - 15/20",
+            "24.08 - Sunny - 20/22",
+            "21.08 - Windy - 26/28",
+            "22.08 - Sunny - 28/30",
+            "23.08 - Storm - 15/20",
+            "24.08 - Sunny - 20/22"
+        };
+
+        ArrayAdapter<String> forecastsAdapter =
+                new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    forecasts);
+
+        ListView forecastListView = (ListView) fragmentView.findViewById(R.id.listview_forecast);
+
+        forecastListView.setAdapter(forecastsAdapter);
+
+        return fragmentView;
     }
 }
