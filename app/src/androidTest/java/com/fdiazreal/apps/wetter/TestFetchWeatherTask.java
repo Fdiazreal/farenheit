d0846ccb897f4c4ad1c3b9af9253fce445bb70a8
Federico Diaz Real <f.diazreal@gmail.com>
2015-10-10 17:59:01 -0300

    Adding code provided by Udacity. Shitty way to learn...

---
+++ app/src/androidTest/java/com/fdiazreal/apps/wetter/TestFetchWeatherTask.java
@@ -0,0 +1,7 @@
+package com.fdiazreal.apps.wetter;
+
+/**
+ * Created by fdiaz on 10/10/2015.
+ */
+public class TestFetchWeatherTask {
+}
---
+++ app/src/main/java/com/fdiazreal/apps/wetter/data/WeatherProvider.java
@@ -0,0 +1,7 @@
+package com.fdiazreal.apps.wetter.data;
+
+/**
+ * Created by fdiaz on 10/10/2015.
+ */
+public class WeatherProvider {
+}
